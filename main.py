78b47e86917291084118da83a2d62deef916cf95
chalkboardninja <139017393+chalkboardninja@users.noreply.github.com>
2024-01-26 16:36:41 -0500

    Added LEDS!
    i added support for LEDS (so that it works in vanilla CM2PY) and also removed a few unnessecary variables

--- main.py
+++ main.py
@@ -4,29 +4,30 @@ from cv2 import imread
 from tkinter.filedialog import askopenfilename
 import cm2py as cm2
 
-fine = 0.0234375
-unfine = 16
 code = ""
 save = cm2.Save()
 
 print("Image Reader!")
 side = ""
+blok = ""
 while not side in ["flat", "top"]:
     side = input("Flat on Baseplate or Standing Up (flat/top): ").lower()
+while not blok in ["led", "tile"]:
+    blok = input("LED Block (works with vanilla CM2PY) or Tile Block (requires modded code in CM2PY)? (led/tile): ").lower()
 input("Press Enter to select a file.")
 image_path = askopenfilename()
 print("Picked " + str(image_path) + "...")
 print("Reading...", end=" ")
 image = imread(image_path)
 print("Done")
-print("Converting...", end=" ")
+print("Converting blocks...", end=" ")
 blocks = []
 x,y=0,0
 for row in image:
     for pixel in row:
         blocks.append(
             save.addBlock(
-                14,
+                [14 if blok == "tile" else 6][0],
                 (x,
                  [(len(image) - 1) - y if side == "top" else 0][0],
                  [(len(image) - 1) - y if side == "flat" else 0][0]
@@ -42,9 +43,20 @@ for row in image:
         x += 1
     y += 1
     x = 0
+print("Done\nAdding connections...", end=" ")
+t_flip_flop = save.addBlock(5, (-1,0,0), True)
+for block in blocks:
+    save.addConnection(t_flip_flop, block)
 print("Done")
 
 
 code = save.exportSave()
 ppc.copy(code)
-print("Copied code to clipboard! :D")
+if code != "":
+    print("Copied code to clipboard! :D")
+else:
+    print("Error exporting save... :(")
+print("====SETTINGS====")
+print("Side: " + side)
+print("Type: " + blok)
+print("Image: " + image_path)
